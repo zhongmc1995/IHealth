7db1262fea75206b689fe2a7f77bb2c177434d2e
lhdzzz <416546473@qq.com>
2017-05-04 22:23:18 +0800

    完成sys_menu表相关的设计

---
+++ src/main/java/com/onion/controller/SysRoleController.java
@@ -0,0 +1,11 @@
+package com.onion.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by lhd on 2017/5/4.
+ * 角色controller
+ */
+@Controller
+public class SysRoleController {
+}
