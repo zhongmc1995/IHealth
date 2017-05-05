abf720ee3ab4d0c1d483c64540d9a78db52553ee
zhongmc <zhongmc_me@163.com>
2017-05-05 08:51:00 +0800

    修改了applicationContext-dao.xml中jdbc properties的引入

---
+++ src/main/java/com/onion/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.onion.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by zhongmc on 2017/5/4.
+ */
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @RequestMapping("/test")
+    public String test(){
+        return "test";
+    }
+}
